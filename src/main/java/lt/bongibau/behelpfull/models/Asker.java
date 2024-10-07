72eac0e2d2bf234f5460346479c2a66a250c7794
Liiiiiison <lisongautron@gmail.com>
2024-10-07 16:10:52 +0200

    User models

---
+++ src/main/java/lt/bongibau/behelpfull/models/Asker.java
@@ -0,0 +1,36 @@
+package lt.bongibau.behelpfull.models;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Asker extends User {
+
+    private LocalDate dateDeNaissance;
+
+    private int validatorId ;
+    public Asker(int id, String username, String password, Date date) {
+        super(id, username, password);
+        this.dateDeNaissance = date.toLocalDate() ;
+    }
+
+    public int getAge(){
+        LocalDate today = LocalDate.now() ;
+        if ((dateDeNaissance != null) && (dateDeNaissance.isBefore(today))) {
+            return Period.between(dateDeNaissance,today).getYears();
+        } else {
+            return 0; // si la date de naissance est incorrecte
+        }
+    }
+
+    public int getValidatorId() {
+        return validatorId;
+    }
+
+    public void setValidatorId(int validatorId) {
+        this.validatorId = validatorId;
+    }
+
+    //    public Request postRequest(){}
+//    public Request deleteRequest(){}
+}
