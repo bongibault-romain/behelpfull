7db6020fca687973a72d299a59435410b7f379fd
Liiiiiison <lisongautron@gmail.com>
2024-10-07 16:49:15 +0200

    asker : validatorId

--- src/main/java/lt/bongibau/behelpfull/models/Asker.java
+++ src/main/java/lt/bongibau/behelpfull/models/Asker.java
@@ -9,9 +9,10 @@ public class Asker extends User {
     private LocalDate dateDeNaissance;
     private int validatorId;
 
-    public Asker(int id, String username, String password, Date date) {
+    public Asker(int id, String username, String password, Date date, int validatorId) {
         super(id, username, password);
         this.dateDeNaissance = date.toLocalDate();
+        this.validatorId = validatorId;
     }
 
     public int getAge() {
