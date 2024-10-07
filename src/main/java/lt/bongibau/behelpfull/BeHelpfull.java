6f4fe854041874899ffd71c29f7132fc4c4a5edc
Bongibault Romain <bongibault.romain@gmail.com>
2024-10-07 15:29:46 +0200

    fix: remove tests

--- src/main/java/lt/bongibau/behelpfull/BeHelpfull.java
+++ src/main/java/lt/bongibau/behelpfull/BeHelpfull.java
@@ -1,15 +1,7 @@
 package lt.bongibau.behelpfull;
 
-import javax.swing.*;
-
 public class BeHelpfull {
     public static void main(String[] args) {
         System.out.println("Hello, BeHelpfull!");
-
-        JFrame frame = new JFrame("BeHelpfull");
-
-        frame.setSize(200, 200);
-
-        frame.setVisible(true);
     }
 }
