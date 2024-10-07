ec3f66c89508903931baac31b47ecff2e1ede69f
Liiiiiison <lisongautron@gmail.com>
2024-10-07 18:06:41 +0200

    authentication

--- src/main/java/lt/bongibau/behelpfull/authentication/Authentication.java
+++ src/main/java/lt/bongibau/behelpfull/authentication/Authentication.java
@@ -1,10 +1,12 @@
 package lt.bongibau.behelpfull.authentication;
 
 import lt.bongibau.behelpfull.exceptions.AuthenticationFailedException;
-import lt.bongibau.behelpfull.exceptions.CreateAskerErrorException;
+import lt.bongibau.behelpfull.exceptions.CreateUserErrorException;
 import lt.bongibau.behelpfull.exceptions.UserExistsException;
 import lt.bongibau.behelpfull.models.Asker;
 import lt.bongibau.behelpfull.models.User;
+import lt.bongibau.behelpfull.models.Validator;
+import lt.bongibau.behelpfull.models.Volunteer;
 import lt.bongibau.behelpfull.users.UserManager;
 import org.jetbrains.annotations.Nullable;
 
@@ -38,9 +40,9 @@ public class Authentication {
             }
             asker = UserManager.getInstance().createAsker(username, password, date, validatorId);
             if (asker == null) {
-                throw (new CreateAskerErrorException("Create Asker Error"));
+                throw (new CreateUserErrorException("Create Asker Error"));
             }
-        } catch (SQLException | UserExistsException | CreateAskerErrorException e) {
+        } catch (SQLException | UserExistsException | CreateUserErrorException e) {
             throw new RuntimeException(e);
         }
 
@@ -49,6 +51,44 @@ public class Authentication {
 
     public Asker registerAsker(String username, String password, Date date) {
         return this.registerAsker(username, password, date, null);
+    }
+
+    public Volunteer registerVolunteer(String username, String password, Date date, boolean hasDriverLicense, boolean hasPSC1) {
+        User user = null;
+        Volunteer volunteer = null;
+        try {
+            user = UserManager.getInstance().getUser(username);
+            if (user != null) {
+                throw (new UserExistsException("Already existing user"));
+            }
+            volunteer = UserManager.getInstance().createVolunteer(username, password, date, hasDriverLicense, hasPSC1);
+            if (volunteer == null) {
+                throw (new CreateUserErrorException("Create Volunteer Error"));
+            }
+        } catch (SQLException | UserExistsException | CreateUserErrorException e) {
+            throw new RuntimeException(e);
+        }
+
+        return volunteer;
+    }
+
+    public Validator registerVolunteer(String username, String password) {
+        User user = null;
+        Validator validator = null;
+        try {
+            user = UserManager.getInstance().getUser(username);
+            if (user != null) {
+                throw (new UserExistsException("Already existing user"));
+            }
+            validator = UserManager.getInstance().createValidator(username, password);
+            if (validator == null) {
+                throw (new CreateUserErrorException("Create Validator Error"));
+            }
+        } catch (SQLException | UserExistsException | CreateUserErrorException e) {
+            throw new RuntimeException(e);
+        }
+
+        return validator;
     }
 
 }
