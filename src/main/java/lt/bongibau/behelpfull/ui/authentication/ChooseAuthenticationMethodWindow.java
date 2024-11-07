c078de4c2bf9c6e2e09cf5ff7580b22f451be4ed
lison <lisongautron@gmail.com>
2024-11-07 10:14:00 +0100

    register button

--- src/main/java/lt/bongibau/behelpfull/ui/authentication/ChooseAuthenticationMethodWindow.java
+++ src/main/java/lt/bongibau/behelpfull/ui/authentication/ChooseAuthenticationMethodWindow.java
@@ -25,6 +25,11 @@ public class ChooseAuthenticationMethodWindow implements Window {
         });
 
         JButton registerButton = new JButton("Register as Asker");
+
+        registerButton.addActionListener(e -> {
+            WindowManager.getInstance().setCurrentWindow(new RegisterAsAskerWindow());
+        });
+
         JButton registerValidatorButton = new JButton("Register as Validator");
         JButton regiserVolunteerButton = new JButton("Register as Volunteer");
 
