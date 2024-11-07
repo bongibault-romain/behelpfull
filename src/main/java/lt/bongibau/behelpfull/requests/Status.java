0cc7256ca583248272013b18a01f63ad1bcb239a
lison <lisongautron@gmail.com>
2024-11-07 09:47:53 +0100

    getStatusOfMyRequests et getFeedbacksOf(Volunteer) pour Asker getList of Validator in order to choose one

--- src/main/java/lt/bongibau/behelpfull/requests/Status.java
+++ src/main/java/lt/bongibau/behelpfull/requests/Status.java
@@ -4,6 +4,7 @@ public enum Status {
 
     PUBLISHED,
     WAITING_FOR_APPROVAL,
+    BANNED_HAMMER,
     ASSIGNED,
     DONE
 }
--- src/main/java/lt/bongibau/behelpfull/users/Volunteer.java
+++ src/main/java/lt/bongibau/behelpfull/users/Volunteer.java
@@ -199,4 +199,6 @@ public class Volunteer extends User {
 
         return requests;
     }
+
+    
 }
